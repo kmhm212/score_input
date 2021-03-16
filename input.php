ad9f44cfc937f9bb2082b28c5b5af1510760075c
kmhm212 <hmhm_auau@yahoo.co.jp>
2021-03-16 22:14:55 +0900

    提出3回目

--- input.php
+++ input.php
@@ -1,22 +1,16 @@
 <?php
 $err_messages = [];
-$tpcs = [
-    'name' => '名前',
-    'japanese' => '国語の点数',
-    'mathematics' => '数学の点数',
-    'science' => '理科の点数',
-    'social_studies' => '社会の点数',
-    'english' => '英語の点数',
+$topics = [
+    'topic'   => ['name', 'japanese',  'mathematics', 'science',  'social_studies', 'english'  ],
+    'subject' => ['名前',  '国語の点数',  '数学の点数',   '理科の点数', '社会の点数',       '英語の点数'],
+    'point'   => ['','','','','',''],
 ];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    foreach ($tpcs as $tpc => $sb) {
-        $topics[$tpc] = $_POST[$tpc];
-        if (empty($topics[$tpc]) && $topics[$tpc] == "") {
-            $err_messages[] = $sb . 'を入力して下さい';
+    for ($i = 0; $i <= 5; $i++) { 
+        $topics['point'][$i] = $_POST[$topics['topic'][$i]];
+        if ($topics['point'][$i] == '') {
+            $err_messages[] = $topics['subject'][$i] . 'を入力して下さい';
         }
-    }
-    if (empty ($err_messages)) {
-        header("Location: result.php");
     }
 }
 ?>
@@ -48,27 +42,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form action="" method="post">
             <div class="form-group">
                 <label for="name">氏名 <span class="required">(必須)<span></label>
-                <input type="text" name="name" id="name" value="<?= $topics['name'] ?>" >
+                <input type="text" name="name" id="name" value="<?= $topics['point'][0] ?>" >
             </div>
             <div class="form-group">
                 <label for="japanese">国語 <span class="required">(必須)<span></label>
-                <input type="number" name="japanese" id="japanese" min="0" max="100" value="<?= $topics['japanese'] ?>">点
+                <input type="number" name="japanese" id="japanese" min="0" max="100" value="<?= $topics['point'][1] ?>">点
             </div>
             <div class="form-group">
                 <label for="mathematics">数学 <span class="required">(必須)<span></label>
-                <input type="number" name="mathematics" id="mathematics" min="0" max="100" value="<?= $topics['mathematics'] ?>">点
+                <input type="number" name="mathematics" id="mathematics" min="0" max="100" value="<?= $topics['point'][2] ?>">点
             </div>
             <div class="form-group">
                 <label for="science">理科 <span class="required">(必須)<span></label>
-                <input type="number" name="science" id="science" min="0" max="100" value="<?= $topics['science'] ?>">点
+                <input type="number" name="science" id="science" min="0" max="100" value="<?= $topics['point'][3] ?>">点
             </div>
             <div class="form-group">
                 <label for="social_studies">社会 <span class="required">(必須)<span></label>
-                <input type="number" name="social_studies" id="social_studies" min="0" max="100" value="<?= $topics['social_studies'] ?>">点
+                <input type="number" name="social_studies" id="social_studies" min="0" max="100" value="<?= $topics['point'][4] ?>">点
             </div>
             <div class="form-group">
                 <label for="english">英語 <span class="required">(必須)<span></label>
-                <input type="number" name="english" id="english" min="0" max="100" value="<?= $topics['english'] ?>">点
+                <input type="number" name="english" id="english" min="0" max="100" value="<?= $topics['point'][5] ?>">点
             </div>
             <input type="submit" value="送信" class="btn">
         </form>
